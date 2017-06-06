1b0c3435320230384015279febfb25755ac8ed29
JayWebDevCom <jaiye.s@gmail.com>
2017-06-06 20:42:33 +0100

    weather API request made and response successfully received

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class Weather extends Component {
 
 componentWillMount() {
-
+const requestApi = 'https://api.apixu.com/v1/current.json?key=ceaa0f0125d04d5da98191845170606&q=london';
+state = { items: [] }; // initial or empty state
+  axios.get(requestApi)
+  .then(response => this.setState({ items: response.data }));
 }
 
 render() {
+  // console.log('asdf', this.state.items.current.feelslike_c)
   const { viewStyle } = styles;
     return (
     <View style={viewStyle}>
