1b66f29dba63c50ccdae8382471e4950777685c8
JayWebDevCom <jaiye.s@gmail.com>
2017-06-07 15:35:46 +0100

    API request using fetch()

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,31 +3,33 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class Weather extends Component {
-state = { items: {} }; // initial or empty state
+  state = { items: {} };
 
-componentWillMount() {
-const requestApi = 'https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1f3365447a2494cb8b115449170706&q=London&format=json&num_of_days=5';
+  componentWillMount() {
+      fetch('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1f3365447a2494cb8b115449170706&q=London&format=json&num_of_days=5')
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          items: responseData,
+        })
+        .done();
+      })
 
-  axios.get(requestApi)
+  }
 
-  .then(
-    response => this.setState({ items: response.data }));
-    console.log(this)
-}
+    renderWeather() {
+      console.log('wdata', this.state)
+      // return <Text>{this.state.items.data.weather[0].date}</Text>
+    }
 
-renderWeather() {
+    render() {
+      return (
 
-  return <Text>{this.state.items.data.weather[0].date}</Text>
-}
+        <View>{this.renderWeather()}</View>
 
-render() {
+      )
 
-return (
-  <View>{this.renderWeather()}</View>
+    }
+  }
 
-)
-
-}
-}
-
-export default Weather;
+  export default Weather;
