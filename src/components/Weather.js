a99ee4d53bdba8a3bb0c59c4de631ad7c40051ba
Alex Law <agent_law@hotmail.com>
2017-06-07 18:56:43 +0100

    Grey boarder styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import axios from 'axios';
 
 class Weather extends Component {
@@ -31,13 +31,27 @@ class Weather extends Component {
 
     render() {
       return (
-        <View>
-        <Text>The date for the weather request: {this.renderDate()}</Text>
-        <Text>The maxium temerature for this day is: {this.renderWeather()} &deg;C</Text>
+        <View style={styles.container}>
+          <View style={styles.content}>
+            <Text>The date for the weather request: {this.renderDate()}</Text>
+            <Text>The maxium temerature for this day is: {this.renderWeather()} &deg;C</Text>
+          </View>
         </View>
       )
 
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+      backgroundColor: '#bdc3c7'
+    },
+    content: {
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center'
+    }
+  });
 
   export default Weather;
