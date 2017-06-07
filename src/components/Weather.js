4c13db912a5b21a0b1ba889943e0f778cb850eaf
JayWebDevCom <jaiye.s@gmail.com>
2017-06-07 13:04:21 +0100

    working version using new api endpoint

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,23 +3,28 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class Weather extends Component {
+state = { items: {} }; // initial or empty state
 
 componentWillMount() {
-const requestApi = 'https://api.apixu.com/v1/current.json?key=ceaa0f0125d04d5da98191845170606&q=london';
+const requestApi = 'https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1f3365447a2494cb8b115449170706&q=London&format=json&num_of_days=5';
 
-state = { items: {} }; // initial or empty state
   axios.get(requestApi)
-  .then(response => this.setState({ items: response.data }));
+
+  .then(
+    response => this.setState({ items: response.data }));
+    console.log(this)
 }
 
 renderWeather() {
-  return <Text>{this.state.items.current.feelslike_c}</Text>
+
+  return <Text>{this.state.items.data.weather[0].date}</Text>
 }
 
 render() {
 
 return (
   <View>{this.renderWeather()}</View>
+
 )
 
 }
